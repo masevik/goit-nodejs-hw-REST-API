8fa5cf637d3ef4d53e7c6b92ae8faa2ae1291e78
Serhii Malychkovych <masevik@gmail.com>
2023-02-17 19:41:19 +0200

    add messages

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -10,27 +10,65 @@ const {
 
 router.get("/", async (req, res, next) => {
   const result = await listContacts();
-  return res.json(result);
+  return res.status(200).json(result);
 });
 
 router.get("/:contactId", async (req, res, next) => {
   const result = await getContactById(Number(req.params.contactId));
-  return res.json(result);
+  if (!result) {
+    next();
+    return;
+  }
+
+  return res.status(200).json(result);
 });
 
 router.post("/", async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: "Missing required name field" });
+  }
+  if (!email) {
+    return res.status(400).json({ message: "Missing required email field" });
+  }
+  if (!phone) {
+    return res.status(400).json({ message: "Missing required phone field" });
+  }
+
   const result = await addContact(req.body);
-  return res.json(result);
+  return res.status(201).json(result);
 });
 
 router.put("/:contactId", async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: "Missing required name field" });
+  }
+  if (!email) {
+    return res.status(400).json({ message: "Missing required email field" });
+  }
+  if (!phone) {
+    return res.status(400).json({ message: "Missing required phone field" });
+  }
+
   const result = await updateContact(Number(req.params.contactId), req.body);
-  return res.json(result);
+
+  if (!result) {
+    next();
+    return;
+  }
+
+  return res.status(200).json(result);
 });
 
 router.delete("/:contactId", async (req, res, next) => {
   const result = await removeContact(Number(req.params.contactId));
-  return res.json(result);
+  if (!result) {
+    next();
+    return;
+  }
+
+  return res.status(200).json(result);
 });
 
 module.exports = router;

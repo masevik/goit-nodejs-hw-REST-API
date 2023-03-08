67b10ed52b27a2a3b056fdf2d8d5406accd706d2
Serhii Malychkovych <masevik@gmail.com>
2023-03-08 14:21:06 +0200

    add pagination and favorite filter

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -3,13 +3,26 @@ const { ctrlWrapper } = require("../../utils");
 
 const getContacts = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "name email");
-  res.status(200).json(result);
+
+  if (favorite === "true" || favorite === "false") {
+    const result = await Contact.find(
+      { owner, favorite },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit,
+      }
+    ).populate("owner", "name email");
+    res.status(200).json(result);
+  } else {
+    const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+      skip,
+      limit,
+    }).populate("owner", "name email");
+    res.status(200).json(result);
+  }
 };
 
 module.exports = {
